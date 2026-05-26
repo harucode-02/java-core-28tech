2649dbd02edfd9a75adff47642c9844feb2ba65b
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-05-26 16:05:11 +0700

    Hello World!

---
+++ src/javademo28tech/Lesson01.java
@@ -0,0 +1,13 @@
+package javademo28tech;
+
+import java.util.Scanner;
+
+public class Lesson01 {
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        System.out.println(x);
+        System.out.println("Hello World !");
+        System.out.println("28tech C++ programming !");
+    }
+}
