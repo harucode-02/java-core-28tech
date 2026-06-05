29e42b4417a78c370c839f07777664f207b15a08
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-05 15:30:53 +0700

    feat: valid triangle checker-(02)

---
+++ src/javademo28tech/Lesson11.java
@@ -0,0 +1,30 @@
+package javademo28tech;
+
+import java.util.Scanner;
+
+public class Lesson11 {
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        if ((a > 0 && b > 0 && c > 0) && (a + b > c && b + c > a && c + a > b)) {
+            if(a == b && b == c){
+                System.out.println(1);
+            } else if (a == b || b == c || c == a) {
+                System.out.println(2);
+            } else if (((a > b && a > c) && (a * a == b * b + c * c)) ||
+                       ((b > a && b > c) && (b * b == a * a + c * c)) ||
+                       ((c > a && c > b) && (c * c == b * b + a * a))){
+                System.out.println(3);
+            } else {
+                System.out.println(4);
+            }
+        } else {
+            System.out.println("INVALID");
+        }
+        scanner.close();
+    }
+}
