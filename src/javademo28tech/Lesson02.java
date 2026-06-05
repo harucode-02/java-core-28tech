abd2239a769f061994a03957e1bbf235b1e1cb24
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-05 12:33:06 +0700

    Initial commit: simple expression evaluator (2) in Java

--- src/javademo28tech/Lesson02.java
+++ src/javademo28tech/Lesson02.java
@@ -5,15 +5,10 @@ import java.util.Scanner;
 public class Lesson02 {
     static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int x = scanner.nextInt();
-        long y = scanner.nextLong();
-        char c = scanner.next().charAt(0);
-        float f = scanner.nextFloat();
-        double d = scanner.nextDouble();
-        System.out.println(x);
-        System.out.println(y);
-        System.out.println(c);
-        System.out.printf("%.2f\n", f);
-        System.out.printf("%.9f\n", d);
+        long a = scanner.nextLong();
+        long b = scanner.nextLong();
+        long c = scanner.nextLong();
+        System.out.println(a * (b + c) + b * (a + c));
+        scanner.close();
     }
 }
