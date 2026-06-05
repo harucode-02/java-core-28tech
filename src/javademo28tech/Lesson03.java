788de056dbab8b272b796b8c7433a237859c7634
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-05 12:46:06 +0700

    feat: implement temperature converter (Celsius to Fahrenheit)

--- src/javademo28tech/Lesson03.java
+++ src/javademo28tech/Lesson03.java
@@ -5,10 +5,8 @@ import java.util.Scanner;
 public class Lesson03 {
     static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt(), b = scanner.nextInt();
-        int chuVi = 2 * (a + b);
-        long dienTich = 1L * a * b;
-        System.out.println("Chu vi HCN la : " + chuVi);
-        System.out.println("Dien tich HCN la : " + dienTich);
+        int c = scanner.nextInt();
+        System.out.printf("%.2f",(double) c * 9 / 5 + 32);
+        scanner.close();
     }
 }
