56a6ef9df1447e7b3d0564a20e01628d7a015c17
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-05 16:39:01 +0700

    feat: student grade classification (Vietnamese 10-point scale)

---
+++ src/javademo28tech/Lesson14.java
@@ -0,0 +1,27 @@
+package javademo28tech;
+
+import java.util.Scanner;
+
+public class Lesson14 {
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+        double d = scanner.nextDouble();
+
+        double result = ((a + b) * 1 + (c * 2) + (d * 3)) / 7;
+
+        if(result >= 8){
+            System.out.println("GIOI");
+        } else if (result < 8 && result >= 6.5){
+            System.out.println("KHA");
+        } else if (result < 6.5 && result >=5) {
+            System.out.println("TRUNG BINH");
+        } else {
+            System.out.println("YEU");
+        }
+        scanner.close();
+    }
+}
