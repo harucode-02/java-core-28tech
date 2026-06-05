7aedacec6641a00373ad78047509c22a2a2212cb
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-05 17:15:32 +0700

    feat: buy water

---
+++ src/javademo28tech/Lesson15.java
@@ -0,0 +1,24 @@
+package javademo28tech;
+
+import java.util.Scanner;
+
+public class Lesson15 {
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        long n = scanner.nextLong();
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        if(a * 2 < b){
+            System.out.println(n * a);
+        } else {
+            if(n % 2 == 0){
+                System.out.println((n / 2) * b);
+            } else {
+                System.out.println((n / 2) * b + a);
+            }
+        }
+        scanner.close();
+    }
+}
