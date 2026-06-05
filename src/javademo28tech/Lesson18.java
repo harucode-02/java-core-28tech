48000ad1eb27176dca279318eb4f27983b97e103
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-05 18:36:07 +0700

    feat: basic string case conversion (upper/lower)

---
+++ src/javademo28tech/Lesson18.java
@@ -0,0 +1,19 @@
+package javademo28tech;
+
+import java.util.Scanner;
+
+public class Lesson18 {
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char c = scanner.next().charAt(0);
+        if(c >= 'A' && c <= 'Z'){
+            System.out.println((char)(c + 32));
+        } else if (c >= 'a' && c <= 'z'){
+            System.out.println((char)(c - 32));
+        } else {
+            System.out.println(c);
+        }
+        scanner.close();
+    }
+}
