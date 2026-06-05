55fd72c1baecafd5968adfd0cdafe5ebaa448b88
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-05 17:32:08 +0700

    feat: next character (increment char)

---
+++ src/javademo28tech/Lesson16.java
@@ -0,0 +1,19 @@
+package javademo28tech;
+
+import java.util.Scanner;
+
+public class Lesson16 {
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char c = scanner.next().charAt(0);
+
+        if(c == 'Z' || c == 'z'){
+            System.out.println('a');
+        } else if (c >= 65 && c <= 90 || c >= 97 && c <= 122){
+            System.out.println((char)(Character.toLowerCase(c) + 1));
+        }
+
+        scanner.close();
+    }
+}
