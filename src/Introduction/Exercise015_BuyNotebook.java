5268ad60184aee017d990909e20ff40e22cd8d3c
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-08 13:50:17 +0700

    feat: BuyNotebook

--- src/Introduction/Exercise015_BuyNotebook.java
+++ src/Introduction/Exercise015_BuyNotebook.java
@@ -1,4 +1,16 @@
 package Introduction;
 
+import java.util.Scanner;
+
 public class Exercise015_BuyNotebook {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+        int x = sc.nextInt();
+
+        System.out.printf("SO VO MUA DUOC LA : %d !!!!!", n / x);
+
+        sc.close();
+    }
 }
