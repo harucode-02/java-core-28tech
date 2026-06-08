2e362753af15a6f2f10058e951de2980a7ed46de
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-08 15:37:56 +0700

    feat: DistanceFromOrigin

--- src/Introduction/Exercise024_DistanceFromOrigin.java
+++ src/Introduction/Exercise024_DistanceFromOrigin.java
@@ -1,4 +1,16 @@
 package Introduction;
 
+import java.util.Scanner;
+
 public class Exercise024_DistanceFromOrigin {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        System.out.printf("%.2f", Math.sqrt((long)x * x + (long)y * y));
+
+        sc.close();
+    }
 }
