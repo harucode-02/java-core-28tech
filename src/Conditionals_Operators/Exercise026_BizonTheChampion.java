808516fb5eac0a8ddecf79375807c17fdb490c31
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-08 17:23:45 +0700

    feat: BizonTheChampion

--- src/Conditionals_Operators/Exercise026_BizonTheChampion.java
+++ src/Conditionals_Operators/Exercise026_BizonTheChampion.java
@@ -14,11 +14,20 @@ public class Exercise026_BizonTheChampion {
         int b2 = sc.nextInt();
         int b3 = sc.nextInt();
 
+        int n = sc.nextInt();
+
         int totalCup = a1 + a2 + a3;
         int totalMedal = b1 + b2 + b3;
 
+        int cupShelves = totalCup % 5 == 0 ? totalCup / 5 : totalCup / 5 + 1;
+        int medalShelves = totalMedal % 10 == 0 ? totalMedal / 10 : totalMedal / 10 + 1;
 
-
+        int totalShelves = cupShelves + medalShelves;
+        if (totalShelves <= n) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
 
         sc.close();
     }
