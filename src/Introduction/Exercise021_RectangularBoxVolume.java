c9b4f43d8e60235d9c76d6fb94fcdfe7f38525c8
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-08 15:10:09 +0700

    feat: RectangularBoxVolume

--- src/Introduction/Exercise021_RectangularBoxVolume.java
+++ src/Introduction/Exercise021_RectangularBoxVolume.java
@@ -1,4 +1,19 @@
 package Introduction;
 
+import java.util.Scanner;
+
 public class Exercise021_RectangularBoxVolume {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+
+        long s = a * b * c;
+
+        System.out.println(s);
+
+        sc.close();
+    }
 }
