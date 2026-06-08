cdd1c0f89a02d6a71f5e412687d1950a9df3b872
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-08 15:28:07 +0700

    feat: MoneyExchange

--- src/Introduction/Exercise023_MoneyExchange.java
+++ src/Introduction/Exercise023_MoneyExchange.java
@@ -1,4 +1,16 @@
 package Introduction;
 
+import java.util.Scanner;
+
 public class Exercise023_MoneyExchange {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        System.out.println(n / 500000);
+        System.out.println(n % 500000);
+
+        sc.close();
+    }
 }
