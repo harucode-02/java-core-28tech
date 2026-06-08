efbd1f3282989571dbcf569786ca88ca82cbe8c9
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-08 12:22:41 +0700

    feat: PowFunction

--- src/Introduction/Exercise004_PowFunction.java
+++ src/Introduction/Exercise004_PowFunction.java
@@ -1,4 +1,16 @@
 package Introduction;
 
+import java.util.Scanner;
+
 public class Exercise004_PowFunction {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        System.out.println((long)Math.pow(x, y));
+
+        sc.close();
+    }
 }
