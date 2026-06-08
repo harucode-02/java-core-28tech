0f2f47e7153e308595124119dda4ff2fbfa397fb
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-08 15:19:30 +0700

    feat: SumOfLastDigits

--- src/Introduction/Exercise022_SumOfLastDigits.java
+++ src/Introduction/Exercise022_SumOfLastDigits.java
@@ -1,4 +1,19 @@
 package Introduction;
 
+import java.util.Scanner;
+
 public class Exercise022_SumOfLastDigits {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long x  = sc.nextLong();
+        long y = sc.nextLong();
+        long z = sc.nextLong();
+
+        long t = ((x % 10) + (y % 100) + (z % 1000));
+
+        System.out.println(t);
+
+        sc.close();
+    }
 }
