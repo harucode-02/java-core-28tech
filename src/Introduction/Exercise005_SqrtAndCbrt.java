21f7bbd8eb60614ff92351bce67ad952fcfad38c
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-08 12:23:31 +0700

    feat: SqrtAndCbrt

--- src/Introduction/Exercise005_SqrtAndCbrt.java
+++ src/Introduction/Exercise005_SqrtAndCbrt.java
@@ -1,4 +1,16 @@
 package Introduction;
 
+import java.util.Scanner;
+
 public class Exercise005_SqrtAndCbrt {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.printf("%.2f\n",Math.sqrt(n));
+        System.out.printf("%.3f",Math.cbrt(n));
+
+        sc.close();
+    }
 }
