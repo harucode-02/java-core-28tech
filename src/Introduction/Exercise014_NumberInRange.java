ca48f513bc6ab8c529c31ace86c75a3c0115fe5a
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-08 13:44:35 +0700

    feat: NumberInRange

--- src/Introduction/Exercise014_NumberInRange.java
+++ src/Introduction/Exercise014_NumberInRange.java
@@ -1,4 +1,16 @@
 package Introduction;
 
+import java.util.Scanner;
+
 public class Exercise014_NumberInRange {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+
+        System.out.println(b - a + 1);
+
+        sc.close();
+    }
 }
