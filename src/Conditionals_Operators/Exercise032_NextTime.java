40b26faf90c1837c59f6968f1d01803164a843a3
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-08 19:51:53 +0700

    feat: NextTime

--- src/Conditionals_Operators/Exercise032_NextTime.java
+++ src/Conditionals_Operators/Exercise032_NextTime.java
@@ -1,4 +1,22 @@
 package Conditionals_Operators;
 
+import java.util.Scanner;
+
 public class Exercise032_NextTime {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+
+        int total = h * 28 + m + k;
+        int remain = total % 784;
+        int a = remain / 28;
+        int b = remain % 28;
+
+        System.out.printf("%02dh:%02dm", a, b);
+
+        sc.close();
+    }
 }
