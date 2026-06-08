c3ce02cf9c9edc002de1942a4ee16fdb5da17f35
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-08 14:18:25 +0700

    feat: RectanglePerimeterAndArea

--- src/Introduction/Exercise017_RectanglePerimeterAndArea.java
+++ src/Introduction/Exercise017_RectanglePerimeterAndArea.java
@@ -1,4 +1,20 @@
 package Introduction;
 
+import java.util.Scanner;
+
 public class Exercise017_RectanglePerimeterAndArea {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long d = sc.nextLong();
+        long r = sc.nextLong();
+
+        long cv = (d + r) * 2;
+        long dt = d * r;
+
+        System.out.println("Chu vi HCN la : " + cv);
+        System.out.println("Dien tich HCN la : " + dt);
+
+        sc.close();
+    }
 }
