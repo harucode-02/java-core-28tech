ec434ba102fef529678ec72dc694644488c095e8
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-08 19:10:11 +0700

    feat: HappyNewYear

--- src/Conditionals_Operators/Exercise030_HappyNewYear.java
+++ src/Conditionals_Operators/Exercise030_HappyNewYear.java
@@ -1,4 +1,16 @@
 package Conditionals_Operators;
 
+import java.util.Scanner;
+
 public class Exercise030_HappyNewYear {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+
+        System.out.println((60 * 24) - (60 * h + m));
+
+        sc.close();
+    }
 }
