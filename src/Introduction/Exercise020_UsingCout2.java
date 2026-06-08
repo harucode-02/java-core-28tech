0882f837b62a07d3ea022d7540993fb198fea482
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-08 14:46:46 +0700

    feat: UsingCount2

--- src/Introduction/Exercise020_UsingCout2.java
+++ src/Introduction/Exercise020_UsingCout2.java
@@ -1,4 +1,16 @@
 package Introduction;
 
+import java.util.Scanner;
+
 public class Exercise020_UsingCout2 {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n =  sc.nextInt();
+
+        System.out.printf("%06d\n", n);
+        System.out.println(String.format("%6d", n).replace(' ', '#'));
+
+        sc.close();
+    }
 }
