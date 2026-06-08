11ac64d8a7dfed405ec49a177b436ef0aaca7841
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-08 16:14:06 +0700

    feat: Divisibility

--- src/Introduction/Exercise027_Divisibility.java
+++ src/Introduction/Exercise027_Divisibility.java
@@ -1,4 +1,15 @@
 package Introduction;
 
+import java.util.Scanner;
+
 public class Exercise027_Divisibility {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        System.out.println(n / 28 + 1);
+
+        sc.close();
+    }
 }
