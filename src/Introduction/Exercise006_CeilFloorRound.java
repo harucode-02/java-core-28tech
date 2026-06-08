e0d392344c0766249db474496252ccd01662c9d0
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-08 12:24:06 +0700

    feat: CeilFloorRound

--- src/Introduction/Exercise006_CeilFloorRound.java
+++ src/Introduction/Exercise006_CeilFloorRound.java
@@ -1,4 +1,17 @@
 package Introduction;
 
+import java.util.Scanner;
+
 public class Exercise006_CeilFloorRound {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double x = sc.nextDouble();
+
+        System.out.println((long)Math.floor(x));
+        System.out.println((long)Math.ceil(x));
+        System.out.println((long)Math.round(x));
+
+        sc.close();
+    }
 }
