bed44ed9b2a2ad0ed7000bbebc5dde7311fd9884
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-08 14:36:52 +0700

    feat: DecimalPart

--- src/Introduction/Exercise019_DecimalPart.java
+++ src/Introduction/Exercise019_DecimalPart.java
@@ -1,4 +1,16 @@
 package Introduction;
 
+import java.util.Scanner;
+
 public class Exercise019_DecimalPart {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double f = sc.nextDouble();
+
+        System.out.println((long)f);
+        System.out.printf("%.2f", f - (long)f % 100);
+
+        sc.close();
+    }
 }
