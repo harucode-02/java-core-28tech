0b0e96ac426ef18faad7dc07006e1365a8378ea7
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-08 15:50:08 +0700

    feat: NumberDivisibleBy3Or5

--- src/Introduction/Exercise025_NumberDivisibleBy3Or5.java
+++ src/Introduction/Exercise025_NumberDivisibleBy3Or5.java
@@ -1,4 +1,19 @@
 package Introduction;
 
+import java.util.Scanner;
+
 public class Exercise025_NumberDivisibleBy3Or5 {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        long a = n / 3;
+        long b = n / 5;
+        long c = n / 15;
+
+        System.out.printf("%d", a + b - c);
+
+        sc.close();
+    }
 }
