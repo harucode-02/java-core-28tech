365690b44ee96f2aacefba81abbca4b147ea8e96
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-08 12:35:09 +0700

    feat: LastDigitAndLastTwoDigits

--- src/Introduction/Exercise007_LastDigitAndLastTwoDigits.java
+++ src/Introduction/Exercise007_LastDigitAndLastTwoDigits.java
@@ -1,4 +1,16 @@
 package Introduction;
 
+import java.util.Scanner;
+
 public class Exercise007_LastDigitAndLastTwoDigits {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        System.out.println(n % 10);
+        System.out.println(n % 100);
+
+        sc.close();
+    }
 }
