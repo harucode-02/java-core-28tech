c5061964625e972caa8c0405b5a2b15ddcede704
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-09 12:30:14 +0700

    feat: ElectricityBillCalculation

--- src/Conditionals_Operators/Exercise035_ElectricityBillCalculation.java
+++ src/Conditionals_Operators/Exercise035_ElectricityBillCalculation.java
@@ -1,4 +1,23 @@
 package Conditionals_Operators;
 
+import java.util.Scanner;
+
 public class Exercise035_ElectricityBillCalculation {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        if(n >= 1000) {
+            System.out.println((n * 4500) + 1200000);
+        } else if (n >= 800) {
+            System.out.println((n * 3900) + 900000);
+        } else if (n >= 500) {
+            System.out.println((n * 3700) + 800000);
+        } else {
+            System.out.println((n * 3300));
+        }
+
+        sc.close();
+    }
 }
