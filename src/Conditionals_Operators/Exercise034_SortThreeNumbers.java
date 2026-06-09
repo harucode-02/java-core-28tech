09f65953467578f8a4bc70332efa27b9f35dd847
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-09 12:19:49 +0700

    feat: SortThreeNumbers

--- src/Conditionals_Operators/Exercise034_SortThreeNumbers.java
+++ src/Conditionals_Operators/Exercise034_SortThreeNumbers.java
@@ -1,4 +1,21 @@
 package Conditionals_Operators;
 
+import java.util.Scanner;
+
 public class Exercise034_SortThreeNumbers {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int min = Math.min(Math.min(a, b), c);
+        int max = Math.max(Math.max(a, b), c);
+        int mid = a + b + c - min - max;
+
+        System.out.println(min + " " + mid + " " + max);
+
+        sc.close();
+    }
 }
