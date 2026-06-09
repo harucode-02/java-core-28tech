6824590a2e769400600c38dc18e9a2af0639c868
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-09 15:54:33 +0700

    feat: DigitalRoot

--- src/Conditionals_Operators/Exercise040_DigitalRoot.java
+++ src/Conditionals_Operators/Exercise040_DigitalRoot.java
@@ -6,8 +6,9 @@ public class Exercise040_DigitalRoot {
     static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        long n = sc.nextLong();
 
-
+        System.out.println(n % 9 == 0 ? 9 : n % 9);
 
         sc.close();
     }
