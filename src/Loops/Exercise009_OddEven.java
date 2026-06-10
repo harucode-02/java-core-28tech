9f6b8445c35d8a57fb12f9fc8cca2e154fe08728
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-10 16:28:14 +0700

    Feat: OddEven

--- src/Loops/Exercise009_OddEven.java
+++ src/Loops/Exercise009_OddEven.java
@@ -1,4 +1,28 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class Exercise009_OddEven {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+        n = Math.abs(n);
+
+        long countOdd = 0;
+        long countEven = 0;
+
+        do {
+            if ((n % 10) % 2 == 0) {
+                countEven++;
+            } else {
+                countOdd++;
+            }
+            n = n / 10;
+
+        } while (n > 0);
+        System.out.println(countOdd < countEven ? "28tech" : "29tech");
+
+        sc.close();
+    }
 }
