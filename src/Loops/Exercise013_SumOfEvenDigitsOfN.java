4f91c8136ce042b247ba9de8e9ad689a9cbfa282
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-10 18:36:55 +0700

    Feat: SumOfEvenDigitsOfN

--- src/Loops/Exercise013_SumOfEvenDigitsOfN.java
+++ src/Loops/Exercise013_SumOfEvenDigitsOfN.java
@@ -1,4 +1,29 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class Exercise013_SumOfEvenDigitsOfN {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        if (n >= 0) {
+            long digit;
+            long sumEven = 0;
+
+            while(n > 0){
+                digit = n % 10;
+                if(digit % 2 == 0){
+                    sumEven += digit;
+                }
+                n = n / 10;
+            }
+            System.out.println(sumEven);
+        } else {
+            System.out.println("NEGATIVE");
+        }
+
+        sc.close();
+    }
 }
