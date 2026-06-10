510ddfc1b5737179711f8dc7727706febbbf5bee
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-10 19:37:07 +0700

    Feat: SumOfDivisorsEasy

--- src/Loops/Exercise016_SumOfDivisorsEasy.java
+++ src/Loops/Exercise016_SumOfDivisorsEasy.java
@@ -1,4 +1,22 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class Exercise016_SumOfDivisorsEasy {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        for(int i = 1; i <= n; i++){
+            if(n % i == 0){
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+
+        sc.close();
+    }
 }
