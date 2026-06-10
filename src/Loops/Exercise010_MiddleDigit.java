8372ba3c35fe0a0b2cee46b1c4858564f89bfa62
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-10 17:41:28 +0700

    Feat: MiddleDigit

--- src/Loops/Exercise010_MiddleDigit.java
+++ src/Loops/Exercise010_MiddleDigit.java
@@ -1,4 +1,29 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class Exercise010_MiddleDigit {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        long count = 0;
+        long original = n;
+
+        while (n > 0) {
+            count++;
+            n = n / 10;
+        }
+        if(count % 2 != 0){
+           for(int i = 0; i < count / 2; i++){
+               original = original / 10;
+           }
+            System.out.println(original % 10);
+        }else{
+            System.out.println("NOT FOUND");
+        }
+
+        sc.close();
+    }
 }
