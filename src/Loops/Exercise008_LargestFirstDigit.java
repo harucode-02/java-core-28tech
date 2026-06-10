780c40b47506d90982cf05a2c09e59c2bd610be4
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-10 15:50:26 +0700

    Feat: LargestFirstDigit

--- src/Loops/Exercise008_LargestFirstDigit.java
+++ src/Loops/Exercise008_LargestFirstDigit.java
@@ -1,4 +1,23 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class Exercise008_LargestFirstDigit {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        long largest = n % 10;
+        long digit;
+
+        while(n >= 10){
+            digit = (n / 10) % 10;
+            largest = Math.max(largest, digit);
+            n = n / 10;
+        }
+        System.out.println(n >= largest ? "YES" : "NO");
+
+        sc.close();
+    }
 }
