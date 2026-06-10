d0a3bc6829f2b9a856ed52588f5280b8f7d09beb
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-10 12:28:46 +0700

    Initial: WritingLoopPractice

--- src/Conditionals_Operators/Exercise012_DaysInMonth.java
+++ src/Conditionals_Operators/Exercise012_DaysInMonth.java
@@ -15,7 +15,7 @@ public class Exercise012_DaysInMonth {
             } else if (m == 4 || m == 6 || m == 9 || m == 11) {
                 System.out.println(30);
             } else {
-                if(m == 2 && ((y % 400 == 0) || (y % 4 == 0 && y % 100 != 0))){
+                if(((y % 400 == 0) || (y % 4 == 0 && y % 100 != 0))){
                     System.out.println(29);
                 } else {
                     System.out.println(28);
