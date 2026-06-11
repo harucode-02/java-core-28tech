e7e2eb55da89efbf75d9304371ffc1a14ccab736
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-11 12:45:49 +0700

    Feat: SumOfOdds

--- src/Loops/Exercise023_SumOfOdds.java
+++ src/Loops/Exercise023_SumOfOdds.java
@@ -1,4 +1,19 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class Exercise023_SumOfOdds {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        long sum = 0;
+        for (long i = 1; i <= n; i++) {
+                sum += 2 * i - 1;
+        }
+        System.out.println(sum);
+
+        sc.close();
+    }
 }
