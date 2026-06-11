437b65aefde6453a6a685f9433a28d1a46d11567
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-11 11:52:09 +0700

    Feat: DivisorProduct

--- src/Loops/Exercise019_DivisorProduct.java
+++ src/Loops/Exercise019_DivisorProduct.java
@@ -1,4 +1,22 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class Exercise019_DivisorProduct {
+    static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            int n = sc.nextInt();
+
+            int sumProduct = 1;
+
+            for(int i = 1; i <= n; i++){
+                if (n % i == 0){
+                    sumProduct *= i;
+                }
+            }
+            System.out.println(sumProduct);
+
+            sc.close();
+    }
 }
