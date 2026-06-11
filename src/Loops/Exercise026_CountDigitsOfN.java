795c9c6c106025895547ad4e117468c205ad33c3
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-11 14:05:58 +0700

    Feat: CountDigitsOfN

--- src/Loops/Exercise026_CountDigitsOfN.java
+++ src/Loops/Exercise026_CountDigitsOfN.java
@@ -1,4 +1,22 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class Exercise026_CountDigitsOfN {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+        n = Math.abs(n);
+
+        long count = 0;
+
+        do {
+            count++;
+            n /= 10;
+        } while (n != 0);
+
+        System.out.println(count);
+        sc.close();
+    }
 }
