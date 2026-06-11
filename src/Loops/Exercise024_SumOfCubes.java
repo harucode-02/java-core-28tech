f731c8d1de212803d141db73bc5983da086d63a6
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-11 12:52:37 +0700

    Feat: SumOfCubes

--- src/Loops/Exercise024_SumOfCubes.java
+++ src/Loops/Exercise024_SumOfCubes.java
@@ -1,4 +1,19 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class Exercise024_SumOfCubes {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+        long sumCubes = 0;
+
+        for (long i = 1; i <= n; i++) {
+            sumCubes += (long) i * i * i;
+        }
+        System.out.println(sumCubes);
+
+        sc.close();
+    }
 }
