78f60329f62323e2bc3b7c7a9e792326aa22fa27
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-11 13:19:51 +0700

    Feat: Factorial

--- src/Loops/Exercise025_Factorial.java
+++ src/Loops/Exercise025_Factorial.java
@@ -1,4 +1,20 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class Exercise025_Factorial {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        long factorial = 1;
+
+        for(long i = 1; i <= n; i++){
+            factorial *= i;
+        }
+        System.out.println(factorial);
+
+        sc.close();
+    }
 }
