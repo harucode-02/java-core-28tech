6c701ea29c3d4eff2344b3bf10eb8f6181198086
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-12 16:22:24 +0700

    Feat: SolveEquation

--- src/Loops/Exercise036_SolveEquation.java
+++ src/Loops/Exercise036_SolveEquation.java
@@ -1,4 +1,27 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class Exercise036_SolveEquation {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int n = sc.nextInt();
+
+        boolean check = false;
+
+        for(int i = 0; i <= n / a; i++){
+            if((n - a * i) % b == 0){
+                System.out.println("YES");
+                check = true;
+            }
+        }
+        if(!check) {
+            System.out.println("NO");
+        }
+
+        sc.close();
+    }
 }
