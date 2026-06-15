c18688fe1536739aa932a8f5fc43ffe45ee076c2
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-15 14:50:28 +0700

    Feat: MultipleTestCases

--- src/Loops/Exercise040_MultipleTestCases.java
+++ src/Loops/Exercise040_MultipleTestCases.java
@@ -1,4 +1,22 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class Exercise040_MultipleTestCases {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        int T = Math.abs(t);
+
+        for (int i = 1; i <= T; i++) {
+            int k = sc.nextInt();
+            if(k % 2 == 0){
+                System.out.println("EVEN");
+            } else {
+                System.out.println("ODD");
+            }
+        }
+        sc.close();
+    }
 }
