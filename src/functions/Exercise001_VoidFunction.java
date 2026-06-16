9e41784fa7182477bec8f14e9fe2dd1ec2603a8a
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-16 15:18:06 +0700

    Feat: VoidFunction

--- src/functions/Exercise001_VoidFunction.java
+++ src/functions/Exercise001_VoidFunction.java
@@ -1,4 +1,13 @@
 package functions;
 
 public class Exercise001_VoidFunction {
+    static void display() {
+        System.out.println("Xin chao 28tech");
+        System.out.println("Ham void");
+        System.out.println("28tech.com.vn");
+    }
+
+    static void main(String[] args) {
+        display();
+    }
 }
