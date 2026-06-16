0f470386e44745f01934fac92caae1b4aa0e0cf2
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-16 15:18:06 +0700

    Initial: VoidFunction

--- src/functions/Exercise002_Parameters.java
+++ src/functions/Exercise002_Parameters.java
@@ -1,4 +1,23 @@
 package functions;
 
+import java.util.Scanner;
+
 public class Exercise002_Parameters {
+    static void display(long x, long y, long z) {
+        System.out.println(y + " " + x + " " + z);
+        System.out.println(x + y + z);
+        System.out.println("KET THUC !");
+    }
+
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long x = sc.nextLong();
+        long y = sc.nextLong();
+        long z = sc.nextLong();
+
+        display(x, y, z);
+
+        sc.close();
+    }
 }
