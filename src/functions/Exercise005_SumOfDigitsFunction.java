9c9fcb53369a5ed8ef6dec37c935bab3d6c20403
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-16 15:48:18 +0700

    Feat: SumOfDigitsFunction

--- src/functions/Exercise005_SumOfDigitsFunction.java
+++ src/functions/Exercise005_SumOfDigitsFunction.java
@@ -1,4 +1,24 @@
 package functions;
 
+import java.util.Scanner;
+
 public class Exercise005_SumOfDigitsFunction {
+    static long sum_digit(long n) {
+        long sumDigit = 0;
+        long digit;
+        while (n > 0) {
+            digit = n % 10;
+            sumDigit += digit;
+            n = n / 10;
+        }
+        return sumDigit;
+    }
+
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long a = sc.nextLong();
+        System.out.println(sum_digit(a));
+        sc.close();
+    }
 }
