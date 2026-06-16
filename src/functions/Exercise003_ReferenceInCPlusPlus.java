d0f3a03b8cd6c199efdf31ee4c2b4e0c22a102e2
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-16 15:34:21 +0700

    Feat: ReferenceInCPlusPlus

--- src/functions/Exercise003_ReferenceInCPlusPlus.java
+++ src/functions/Exercise003_ReferenceInCPlusPlus.java
@@ -1,4 +1,19 @@
 package functions;
 
+import java.util.Scanner;
+
 public class Exercise003_ReferenceInCPlusPlus {
+    static long solve(long a) {
+        return a * 28;
+    }
+
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long x = sc.nextLong();
+
+        System.out.println(solve(x));
+
+        sc.close();
+    }
 }
