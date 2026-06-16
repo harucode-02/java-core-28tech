010d3659dbb55b28cc61886f58766943ad1dd8d6
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-16 16:14:11 +0700

    Feat: SumOfDigits2

--- src/functions/Exercise006_SumOfDigits2.java
+++ src/functions/Exercise006_SumOfDigits2.java
@@ -1,4 +1,32 @@
 package functions;
 
+import java.util.Scanner;
+
 public class Exercise006_SumOfDigits2 {
+    static long sum_digit(long n) {
+        long sumDigit = 0;
+        long digit;
+
+            while(n > 0){
+               digit = n % 10;
+               sumDigit += digit;
+               n = n / 10;
+            }
+
+        return sumDigit;
+    }
+
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        long x;
+
+        for(int i = 1; i <= n; i++){
+            x = sc.nextLong();
+            System.out.print(sum_digit(x) + " ");
+        }
+
+        sc.close();
+    }
 }
