7e39677c4f098b6928f9a6c5a8475c314811e200
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-16 16:24:17 +0700

    Feat: ReturnTrueFalse

--- src/functions/Exercise007_ReturnTrueFalse.java
+++ src/functions/Exercise007_ReturnTrueFalse.java
@@ -1,4 +1,31 @@
 package functions;
 
+import java.util.Scanner;
+
 public class Exercise007_ReturnTrueFalse {
+    static boolean check(long n) {
+        long sumDigit = 0;
+        long digit;
+
+        while (n > 0){
+            digit = n % 10;
+            sumDigit += digit;
+            n = n / 10;
+        }
+
+        return sumDigit % 10 == 8;
+    }
+
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long x = sc.nextLong();
+        if(check(x)){
+            System.out.println("28tech");
+        } else {
+            System.out.println("29tech");
+        }
+
+        sc.close();
+    }
 }
