2b74805dc3c164fc9220beb59e596b080ea7a151
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-16 21:44:54 +0700

    Feat: CombineMultipleFunctionResults

--- src/functions/Exercise009_CombineMultipleFunctionResults.java
+++ src/functions/Exercise009_CombineMultipleFunctionResults.java
@@ -1,4 +1,60 @@
 package functions;
 
+import java.util.Scanner;
+
 public class Exercise009_CombineMultipleFunctionResults {
+    static boolean chua6(int n) {
+        int digit;
+        while(n > 0){
+            digit = n % 10;
+            if(digit == 6){
+                return true;
+            }
+            n /= 10;
+        }
+        return false;
+    }
+
+    static boolean tongchan(int n) {
+        int digit;
+        int sumDigit = 0;
+        while(n > 0){
+            digit = n % 10;
+            sumDigit += digit;
+            n /= 10;
+        }
+        return sumDigit % 2 == 0;
+    }
+
+    static boolean chanle(int n) {
+        int digit;
+        int countEven = 0;
+        int countOdd = 0;
+
+        while (n != 0) {
+            digit = n % 10;
+            if (digit % 2 == 0) {
+                countEven++;
+            } else {
+                countOdd++;
+            }
+            n /= 10;
+        }
+        return countOdd > countEven;
+    }
+
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for(int i = a; i <= b ; i++){
+            if(chua6(i) && tongchan(i) && chanle(i)){
+                System.out.print(i + " ");
+            }
+        }
+
+        sc.close();
+    }
 }
--- src/loops/Exercise003_WritingLoopPractice.java
+++ src/loops/Exercise003_WritingLoopPractice.java
@@ -9,31 +9,31 @@ public class Exercise003_WritingLoopPractice {
         int n = sc.nextInt();
 
         // Line 1
-        for(int i = 1; i <= n; i++){
+        for (int i = 1; i <= n; i++) {
             System.out.print(i + " ");
         }
         System.out.println();
 
         // Line 2
-        for(int i = n; i >= 0; i--){
+        for (int i = n; i >= 0; i--) {
             System.out.print(i + " ");
         }
         System.out.println();
 
         // Line 3
-        for(int i = 0; i <= n; i += 2){
+        for (int i = 0; i <= n; i += 2) {
             System.out.print(i + " ");
         }
         System.out.println();
 
         // Line 4
-        for(int i = 1; i <= n; i += 2){
+        for (int i = 1; i <= n; i += 2) {
             System.out.print(i + " ");
         }
         System.out.println();
 
         // Line 5
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             if (i % 4 == 0) {
                 System.out.print(i + " ");
             }
@@ -41,13 +41,13 @@ public class Exercise003_WritingLoopPractice {
         System.out.println();
 
         // Line 6
-        for(char c = 'a'; c < 'a' + n; c++){
+        for (char c = 'a'; c < 'a' + n; c++) {
             System.out.printf("%c ", c);
         }
         System.out.println();
 
         // Line 7
-        for(char c = (char)('z' - n + 1); c <= 'z'; c++){
+        for (char c = (char)('z' - n + 1); c <= 'z'; c++) {
             System.out.printf("%c ", c);
         }
 
