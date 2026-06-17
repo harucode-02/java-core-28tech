4af2caf6fec4a755eb95cb089044f533ea7a6038
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-17 12:56:50 +0700

    Feat: ReverseNumber

--- src/functions/Exercise014_ReverseNumber.java
+++ src/functions/Exercise014_ReverseNumber.java
@@ -1,4 +1,28 @@
 package functions;
 
+import java.util.Scanner;
+
 public class Exercise014_ReverseNumber {
+    static long reverse(long n) {
+        long digit;
+        long result = 0;
+
+        while (n > 0) {
+            digit = n % 10;
+            result = result * 10 + digit;
+            n = n / 10;
+
+        }
+        return result;
+    }
+
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        System.out.println(reverse(n));
+
+        sc.close();
+    }
 }
