f7bffd4859444e26bc2103de2ccf27e06ef254f8
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-17 12:20:02 +0700

    Feat: IncreasingNumber

--- src/functions/Exercise012_IncreasingNumber.java
+++ src/functions/Exercise012_IncreasingNumber.java
@@ -1,4 +1,34 @@
 package functions;
 
+import java.util.Scanner;
+
 public class Exercise012_IncreasingNumber {
+    static boolean so_tang(int n) {
+        int digit;
+        int next;
+
+        while (n >= 10){
+            digit = n % 10;
+            n /= 10;
+            next = n % 10;
+            if(next >= digit){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for(int i = a; i <= b; i++){
+            if(so_tang(i)){
+                System.out.print(i + " ");
+            }
+        }
+        sc.close();
+    }
 }
