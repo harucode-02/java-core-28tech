9d9a65818b35f6a169fab7c5532191ce6b5ca634
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-17 11:51:42 +0700

    Feat: SwapFunction

--- src/functions/Exercise011_SwapFunction.java
+++ src/functions/Exercise011_SwapFunction.java
@@ -1,4 +1,19 @@
 package functions;
 
+import java.util.Scanner;
+
 public class Exercise011_SwapFunction {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        int temp = x;
+        x = y;
+        y = temp;
+
+        System.out.println(x + " " + y);
+        sc.close();
+    }
 }
