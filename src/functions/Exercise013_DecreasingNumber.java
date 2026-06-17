e13f333043c9a3c2faea1174bb09824aa9a52d48
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-17 12:32:45 +0700

    Feat: DecreasingNumber

--- src/functions/Exercise013_DecreasingNumber.java
+++ src/functions/Exercise013_DecreasingNumber.java
@@ -1,4 +1,39 @@
 package functions;
 
+import java.util.Scanner;
+
 public class Exercise013_DecreasingNumber {
+    static boolean so_giam(int n) {
+        int digit;
+        int next;
+
+        while (n >= 10) {
+            digit = n % 10;
+            n /= 10;
+            next = n % 10;
+
+            if(next <= digit) return false;
+        }
+        return true;
+    }
+
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        boolean found = false;
+        for(int i = a; i <= b; i++){
+            if(so_giam(i)){
+                System.out.print(i + " ");
+                found = true;
+            }
+        }
+        if(!found){
+            System.out.println("28tech");
+        }
+
+        sc.close();
+    }
 }
