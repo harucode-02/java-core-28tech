96c2891814d3e0a920f7406f9ef9376050f66e59
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-17 14:19:37 +0700

    Initial: ArrayTraversal

---
+++ src/Array1D/Exercise001_ArrayTraversal.java
@@ -0,0 +1,27 @@
+package Array1D;
+
+import java.util.Scanner;
+
+public class Exercise001_ArrayTraversal {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        for(int i = n - 1; i >= 0; i--){
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
