d4a9a7f105a07217eba0852835e152b388da5a6d
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-17 15:40:15 +0700

    Feat: ProductOfAdjacentElements

---
+++ src/Array1D/Exercise006_ProductOfAdjacentElements.java
@@ -0,0 +1,26 @@
+package Array1D;
+
+import java.util.Scanner;
+
+public class Exercise006_ProductOfAdjacentElements {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < n - 1; i++){
+            arr[i] *= arr[i + 1];
+        }
+
+        for(int i = 0; i < n - 1; i++){
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
