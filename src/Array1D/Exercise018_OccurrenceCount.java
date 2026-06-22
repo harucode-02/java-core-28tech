e2207861478712964e5639b6f8cd3e4fdf76b1f3
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-22 11:38:21 +0700

    Feat: OccurrenceCount

--- src/Array1D/Exercise018_OccurrenceCount.java
+++ src/Array1D/Exercise018_OccurrenceCount.java
@@ -1,4 +1,27 @@
 package Array1D;
 
+import java.util.Scanner;
+
 public class Exercise018_OccurrenceCount {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        char c = sc.next().charAt(0);
+        char[] arr = new char[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.next().charAt(0);
+        }
+
+        int countChar = 0;
+        for(int i = 0; i < n; i++){
+            if(arr[i] == c){
+                countChar++;
+            }
+        }
+        System.out.print(countChar + " ");
+
+        sc.close();
+    }
 }
