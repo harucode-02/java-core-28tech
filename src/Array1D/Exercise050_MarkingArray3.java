9cf5dafaaa43ed057053619c96b779dbdd548a7f
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-24 19:18:02 +0700

    Feat: MarkingArray3

---
+++ src/Array1D/Exercise050_MarkingArray3.java
@@ -0,0 +1,39 @@
+package Array1D;
+
+import java.util.Scanner;
+
+public class Exercise050_MarkingArray3 {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            boolean check = false;
+            for (int j = 0; j <= i - 1; j++) {
+                if (arr[i] == arr[j]) {
+                    check = true;
+                    break;
+                }
+            }
+
+            int count = 0;
+            for (int k = 0; k < n; k++) {
+                if (arr[i] == arr[k]) {
+                    count++;
+                }
+            }
+
+            if (!check) {
+                System.out.println(arr[i] + " " + count);
+            }
+        }
+
+        sc.close();
+    }
+}
