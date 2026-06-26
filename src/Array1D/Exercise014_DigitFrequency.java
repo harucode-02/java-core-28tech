59be3714e7482842919e444737737ae30bd10971
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-26 22:52:07 +0700

    Feat: Revision

--- src/Array1D/Exercise014_DigitFrequency.java
+++ src/Array1D/Exercise014_DigitFrequency.java
@@ -7,25 +7,25 @@ public class Exercise014_DigitFrequency {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        long [] arr = new long[n];
+        long[] arr = new long[n];
 
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.nextLong();
         }
 
-        long [] count = new long[10];
-        for(int i = 0; i < n; i++){
-            if(arr[i] == 0) count[0]++;
-            while(arr[i] > 0) {
+        long[] count = new long[10];
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 0) count[0]++;
+            while (arr[i] > 0) {
                 long temp = arr[i];
                 temp %= 10;
-                count[(int)temp]++;
+                count[(int) temp]++;
                 arr[i] /= 10;
             }
         }
 
-        for(int i = 0; i <= 9; i++){
-            if(count[i] > 0) {
+        for (int i = 0; i <= 9; i++) {
+            if (count[i] > 0) {
                 System.out.println(i + " " + count[i]);
             }
         }
