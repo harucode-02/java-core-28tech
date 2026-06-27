0612dd8649fd344824817084c1bd5ecb53f254c8
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-27 23:26:00 +0700

    Feat: Revision

--- src/Array1D/Exercise054_RotateLeft.java
+++ src/Array1D/Exercise054_RotateLeft.java
@@ -17,11 +17,11 @@ public class Exercise054_RotateLeft {
         int[] temp = new int[k];
         k %= n;
 
-        for(int i = 0; i < k; i++){
+        for (int i = 0; i < k; i++){
             temp[i] = arr[i];
         }
 
-        for(int i = 0; i < n-k; i++){
+        for (int i = 0; i < n-k; i++){
              arr[i] = arr[i + k];
         }
 
