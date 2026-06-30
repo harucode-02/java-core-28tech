1528e87a264b82ea1e0441b7d3aa3e8a77041532
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-30 14:25:45 +0700

    Feat: RotateLeft (adjustment)

--- src/Array1D/Exercise054_RotateLeft.java
+++ src/Array1D/Exercise054_RotateLeft.java
@@ -14,23 +14,17 @@ public class Exercise054_RotateLeft {
             arr[i] = sc.nextInt();
         }
 
-        k %= n; // in case (k>=n)
-        int[] temp = new int[k];
+        k = k % n;
 
-        for (int i = 0; i < k; i++){
-            temp[i] = arr[i];
+        int[] B = new int[n];
+
+        for(int i = 0; i < n; i++){
+            int j = (i - k + n) % n;
+            B[j] = arr[i];
         }
 
-        for (int i = 0; i < n-k; i++){
-             arr[i] = arr[i + k];
-        }
-
-        for(int i = 0; i < n-k; i++){
-            System.out.print(arr[i] + " ");
-        }
-
-        for(int i = 0; i < k; i++){
-            System.out.print(temp[i] + " ");
+        for(int j = 0; j < n; j++){
+            System.out.print(B[j] + " ");
         }
 
         sc.close();
